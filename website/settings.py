44c490cb615c321d059eefb9f43f431fac4cdf13
Josh Cai <josh.cai@yahoo.com>
2013-08-13 02:43:27 -0500

    added gunicorn and stuff

--- website/settings.py
+++ website/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'blog',
     'south', #database migration
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
