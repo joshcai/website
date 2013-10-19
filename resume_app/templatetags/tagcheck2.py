2607f6a7de253a5f3cb363c51719c7d37e7a6e31
Josh Cai <josh.cai@yahoo.com>
2013-10-18 21:41:44 -0500

    fixed resume site

--- resume_app/templatetags/tagcheck2.py
+++ resume_app/templatetags/tagcheck2.py
@@ -2,16 +2,16 @@ from django import template
 
 register = template.Library()
 
-def tagcheck(value, arg):
+def tagcheck2(value, arg):
 	"""Gets the value of the tag in request session"""
 
 	return str(value.id)==str(arg)
 
-register.filter('tagcheck', tagcheck)
+register.filter('tagcheck2', tagcheck2)
 
-def tag(value, arg):
+def tag2(value, arg):
 	if arg in value.tags.all():
 		return True
 	return False
 
-register.filter('tag', tag)
+register.filter('tag2', tag2)
